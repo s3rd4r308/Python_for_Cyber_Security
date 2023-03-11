ce1a4e5850ccb9b7a4a1b0a2628a129c71892bb0
Serdar <125079357+s3rd4r308@users.noreply.github.com>
2023-03-11 18:26:09 -0500

    Add files via upload

---
+++ Password_Generator/password_gen.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import string
+import secrets
+import argparse
+import sys
+from termcolor import colored
+
+parser = argparse.ArgumentParser(prog="password_gen", description="Password Generator and Password Safe Checker")
+parser.add_argument("-l", "--length", help="length of the password", required=True)
+args = parser.parse_args()
+
+print(colored("Only for Educational Purposes\n", "red"))
+print(colored("USERNAME ENUMERATOR BY @s3rd4r308\n", "green"))
+
+length = args.length
+
+lowercases = string.ascii_lowercase
+uppercases = string.ascii_uppercase
+digits = string.digits
+punctuations = string.punctuation
+
+chars = lowercases + uppercases + digits + punctuations
+
+password = ""
+
+for i in range(int(length)):
+	print(colored("[*] Generating The Password...\n", "blue"))
+	password += "".join(secrets.choice(chars))
+
+print(colored("[+] Password Generated", "green"))
+print(colored(f"[+] Password: {password}", "green"))
